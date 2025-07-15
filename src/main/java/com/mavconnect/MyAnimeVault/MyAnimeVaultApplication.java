e5bd99b89aa3408bbbca8dff912ae852857d0afb
thiago.san <thisantosareis@gmail.com>
2025-07-15 09:49:18 -0300

    first commit

---
+++ src/main/java/com/mavconnect/MyAnimeVault/MyAnimeVaultApplication.java
@@ -0,0 +1,13 @@
+package com.mavconnect.MyAnimeVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnimeVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAnimeVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mavconnect/MyAnimeVault/MyAnimeVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavconnect.MyAnimeVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAnimeVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
